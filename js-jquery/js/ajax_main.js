b52e659f4bd84bb47864f72a7c83c2599b37b614
pia222sk20 <pia222.sk20@gmail.com>
2025-12-19 15:07:23 +0900

    .

--- js-jquery/js/ajax_main.js
+++ js-jquery/js/ajax_main.js
@@ -8,8 +8,27 @@ function loadUsers(){
         method: 'GET',
         success:function(users){
             $('#userTable').empty();
-            
+            users.forEach(user => {
+                $('#userTable').append(
+                     $('#userTable').append(
+                    `
+                    <tr data-id="${user.id}">
+                        <td><input type="checkbox" class="chk"></td>
+                        <td>${user.id}</td>
+                        <td>${user.name}</td>
+                        <td>${user.email}</td>
+                        <td>
+                            <button class="edit">MODIFY</button>
+                            <button class="remove">REMOVE</button>
+                        </td>
+                    </tr>
+                    `
+                    )
+                )
+            });
         },
-        error:function(){}
+        error:function(){
+            alert('목록조회 실패');
+        }
     });
 }
