a3059acbe0567336e633e37b50880a8f98ab3e00
pia222sk20 <pia222.sk20@gmail.com>
2025-12-19 10:51:26 +0900

    .

--- js-jquery/js/main.js
+++ js-jquery/js/main.js
@@ -8,5 +8,40 @@ $(document).ready(
         $("#btn").click(function(){
             $('#text').text('버튼 클릭됨');        
         })
+
+        // 전체선택
+        $("#checkall").on('change',function(){
+            $('.chk').prop('checked',this.checked);
+            const checked = $('.chk:checked').length
+            // 개수를 카운트
+            $('#count').text(checked)
+
+        });
+
+        // 개별체크로 전체 컨트롤
+        $('.chk').on('change',function(){
+            const total = $('.chk').length
+            const checked = $('.chk:checked').length
+            $('#checkall').prop('checked',total==checked)
+            // 개수를 카운트
+            $('#count').text(checked)
+
+        });
+
+        // 선택 삭제
+        $('#deleteBtn').click(function(){
+            $('.tchk:checked').each(function(){
+                $(this).closest('tr').remove()
+            });
+        });
+        // 버튼 비활성화(중복 클릭 방지)  저장 결제 api 호출
+        $('#saveBtn').click(function(){
+            $(this).prop('diabled',true)
+
+            setTimeout( ()=>{
+                $(this).prop('disabled',false)
+            },2000  );
+        });
+
     }   
 );
