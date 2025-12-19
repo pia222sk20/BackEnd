5f31e67afbd24f2fd88669aab96cdd21963be983
pia222sk20 <pia222.sk20@gmail.com>
2025-12-19 14:51:00 +0900

    .

---
+++ js-jquery/js/ajax_main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    loadUsers();
+});
+
+function loadUsers(){
+    $.ajax({
+        url:'/api/users', 
+        method: 'GET',
+        success:function(users){
+            $('#userTable').empty();
+            
+        },
+        error:function(){}
+    });
+}
