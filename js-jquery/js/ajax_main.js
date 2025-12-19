01d84600e6e7334716f6b983cb8d8a9b26948790
pia222sk20 <pia222.sk20@gmail.com>
2025-12-19 15:44:33 +0900

    .

--- js-jquery/js/ajax_main.js
+++ js-jquery/js/ajax_main.js
@@ -4,6 +4,15 @@
 
 $(document).ready(function(){
     loadUsers();
+
+    // CREATE 행 추가  prompt
+    $("#addBtn").on('click',function(){
+        const name = prompt('이름 입력');
+        const email = prompt('이메일 입력');
+        if(!name || !email) return;        
+        const user = {name, email}
+        createUser(user);
+    });
 });
 
 function loadUsers(){
@@ -31,6 +40,20 @@ function loadUsers(){
         },
         error:function(){
             alert('목록조회 실패');
+        }
+    });
+}
+
+// POST 사용자 추가
+function createUser(data){
+    $.ajax({
+        url:'http://localhost:3000/users',
+        method: 'POST',
+        contentType : 'application/json',
+        data : JSON.stringify(data),
+        success:function(){
+            alert('등록되었습니다.');
+            loadUsers(); // 목록 갱신
         }
     });
 }
