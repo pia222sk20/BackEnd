facf7c74be43713d7c7941c6a407a1c8a18f8c40
pia222sk20 <pia222.sk20@gmail.com>
2025-12-23 17:31:37 +0900

    .

--- Django_test/blog/urls.py
+++ Django_test/blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'  # 네임스페이스
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     # path('<int:pk>/', views.post_detail, name='post_detail'),
-    # path('create/', views.post_create, name='post_create'),
+    path('create/', views.post_create, name='post_create'),
     # path('<int:pk>/update/', views.post_update, name='post_update'),
     # path('<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
--- Django_test/blog/views.py
+++ Django_test/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
 # Create your views here.
 def post_list(request): 
@@ -14,7 +14,16 @@ def post_list(request):
 def post_detail(request):
     pass
 def post_create(request):
-    pass
+    '''화면에서 post방식으로 전달한 데이터를 가지고 Post 테이블에 저장'''
+    if request.method == 'POST':
+        title = request.POST['title']   #  html에서 name = title
+        content = request.POST['content']  # html에서 name = content
+        post = Post(title=title, content=content)
+        post.save()  # insert 쿼리가 실행
+        return redirect('blog:post_list')  # DB가 갱신되면 새로운 DB에 정보를 가지고 화면을 refresh
+    elif request.method == 'GET':        
+        return render(request, 'blog/post_form.html')
+
 def post_update(request):
     pass
 def post_delete(request):
