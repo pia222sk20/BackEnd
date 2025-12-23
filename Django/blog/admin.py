59adc96bf216abd930940e702fac639ae8720db0
pia222sk20 <pia222.sk20@gmail.com>
2025-12-23 10:46:32 +0900

    .

--- Django/blog/admin.py
+++ Django/blog/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Question, Answer
 # Register your models here.
 admin.site.register(Question)
-admin.site.register(Answer)
+admin.site.register(Answer) 
