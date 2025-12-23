ba208e87acfe838f0d38b3b71422f9c2b46cddad
pia222sk20 <pia222.sk20@gmail.com>
2025-12-23 17:14:25 +0900

    .

--- Django_test/blog/views.py
+++ Django_test/blog/views.py
@@ -6,7 +6,11 @@ def post_list(request):
     # 데이터베이스에서 post 테이블의 데이터를 전부가져와서 html에 전달
     # db를 담당하는 모델을 부른다
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html')
+    print(f'posts: {posts}')    
+    content = {
+        'posts': posts,
+    }
+    return render(request, 'blog/post_list.html',content)
 def post_detail(request):
     pass
 def post_create(request):
