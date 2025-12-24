0a48f1855c34f143a7bd805f810978a85e8509a9
pia222sk20 <pia222.sk20@gmail.com>
2025-12-24 11:28:57 +0900

    .

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -28,4 +28,41 @@ class Post(models.Model):
     # category.posts.all()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL,null=True,
                                  blank=True,related_name='posts')   
+    # 다대다
+    tag = models.ManyToManyField(Tag, blank=True,related_name='posts')
+    created_at = models.DateTimeField(auto_now_add=True)  # 생성 시간 딱 한번만
+    updated_at = models.DateTimeField(auto_now=True) # 수정 시간  자동갱신
+    published = models.BooleanField(default=False)
+    views = models.IntegerField(default=0)  # 조회수
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='comments')
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    class Meta:
+        ordering = ['-created_at']
+    def __str__(self):
+        return f"{self.author.username}'s comment"
+class Bookmark(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='bookmarks')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='bookmarks')
+    created_at = models.DateField(auto_now_add=True)
+    class Meta:  # 같은 유저는 같은 포스트를 한번만 북마크 할수 있다
+        unique_together = ('post', 'user')  # 여러필드를 묶어서 하나의 유니크 제약조건
+    def __str__(self):
+        return f"{self.user.username}- {self.post.title}"
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='likes')
+    created_at = models.DateField(auto_now_add=True)
+    class Meta:  
+        unique_together = ('post', 'user')  # 여러필드를 묶어서 하나의 유니크 제약조건
+    def __str__(self):
+        return f"{self.user.username} - {self.post.title}"
     
