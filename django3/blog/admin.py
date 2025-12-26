bff5c3ab987577e58ac05b70bf5fb19ddde46c95
pia222sk20 <pia222.sk20@gmail.com>
2025-12-26 10:12:54 +0900

    .

--- django3/blog/admin.py
+++ django3/blog/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
+from .models import Category, Tag, Post, Comment
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}  # 어드민 입력에서 자동으로 slug를 생성
+    # name 입력시 slug가 실시간으로 자동 채워짐
+    search_fields = ['name', 'description']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'created_at']
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+    fields = ['author', 'content', 'is_approved', 'created_at']
+    readonly_fields = ['created_at']
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'status', 'views', 'created_at']
+    list_filter = ['status', 'category', 'created_at', 'featured']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ['tags']
+    date_hierarchy = 'created_at'
+    inlines = [CommentInline]
+    
+    fieldsets = (
+        ('기본 정보', {
+            'fields': ('title', 'slug', 'author', 'category', 'tags')
+        }),
+        ('내용', {
+            'fields': ('excerpt', 'content')
+        }),
+        ('설정', {
+            'fields': ('status', 'featured', 'views')
+        }),
+        ('날짜', {
+            'fields': ('published_at',),
+            'classes': ('collapse',)
+        }),
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'author', 'content_preview', 'is_approved', 'created_at']
+    list_filter = ['is_approved', 'created_at']
+    search_fields = ['content', 'author__username', 'post__title']
+    
+    def content_preview(self, obj):
+        return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
+    content_preview.short_description = '내용'
