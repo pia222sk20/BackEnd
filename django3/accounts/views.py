c99270fd5b0dbe4bf598577918e3126314dc380e
pia222sk20 <pia222.sk20@gmail.com>
2025-12-26 14:24:36 +0900

    .

--- django3/accounts/views.py
+++ django3/accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
-
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 # Create your views here.
+class AuthViewSet(viewsets.GenericViewSet):
+    '''인증관련 viewset'''
+    serializer_class = UserSerializer
+
+    # AllowAny : 로그인 없이 접근가능
+    @action(detail=False, methods=['POST'],permission_classes=[AllowAny])
+    def register(self, request):
+        '''회원가입'''
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            # instance가 없으면 create , 있으면 update
+            user = serializer.save()   # 내부 규칙에 의해서 update or create 자동 호출 
+            token,created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token' : token.key,
+                'user':UserSerializer(user).data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    @action(detail=False, methods=['POST'], permission_classes=[AllowAny])
+    def login(self,request):
+        '''로그인'''
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():            
+            user = serializer.validated_data
+            token,created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token' : token.key,
+                'user':UserSerializer(user).data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
