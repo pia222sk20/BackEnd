a9579edf8a5ffb14a6f3b8e83790d6b896c359f3
pia222sk20 <pia222.sk20@gmail.com>
2025-12-29 15:31:36 +0900

    .

--- Django-FastApi-Integration/django-project/products/urls.py
+++ Django-FastApi-Integration/django-project/products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'products'
 urlpatterns = [
-    path('',views.product_list,name='product_list')
+    path('',views.product_list,name='product_list'),
+    path('products/create',views.product_create,name='product_create')
 ]
