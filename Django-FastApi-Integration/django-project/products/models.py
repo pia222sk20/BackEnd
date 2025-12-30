a79dc742bb8fec72ab2e615ccabad0c391e0dce1
pia222sk20 <pia222.sk20@gmail.com>
2025-12-30 15:04:55 +0900

    .

--- Django-FastApi-Integration/django-project/products/models.py
+++ Django-FastApi-Integration/django-project/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    '''사용자 모델 확장'''
+    ROLE_CHOICES = [
+        ('user','일반 사용자'),
+        ('manager','매니저'),
+        ('admin','관리자'),
+    ]
+    role = models.CharField(max_length=20,choices=ROLE_CHOICES,default='user',verbose_name='역활')
+    class Meta:
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자 목록'
+    
