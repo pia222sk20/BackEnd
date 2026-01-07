c8fd645506bc485f1f73a517cc41ca2cacdb19c2
pia222sk20 <pia222.sk20@gmail.com>
2026-01-07 11:30:37 +0900

    .

--- fastapi-mysql-project/app/main.py
+++ fastapi-mysql-project/app/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from app.db.init_db import init_table
 from app.core.config import settings
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 앱 시작시 실행
+    await init_table()
+    yield
+    # 앱 종료시 실행(필요시)
+
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
